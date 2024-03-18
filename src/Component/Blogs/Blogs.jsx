e6c24391e574551c9ec3f7867923b21a4624cb7d
numanahmed2005 <numan.ahmedx@proton.me>
2024-03-19 00:34:39 +0600

    fetch the api done

--- src/Component/Blogs/Blogs.jsx
+++ src/Component/Blogs/Blogs.jsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 
 
 const Blogs = () => {
+    let [ blogs , setblog]= useState([]);
+    useEffect(()=>{
+        fetch('blog.json')
+        .then( res=> res.json())
+        .then( data=> setblog(data))
+    } , [])
     return (
         <div>
             
