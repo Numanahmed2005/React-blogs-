8bd9bc6e460a2c6a47175a51c8d68b3c5c8e1a08
numanahmed2005 <numan.ahmedx@proton.me>
2024-03-18 23:42:39 +0600

    react blog add

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div className="text-3xl">
+            <h1> Knowledge cafe </h1>
+            
+        </div>
+    );
+};
+
+export default Header;
